a681ce0c393e9d965b7ec09d908263e2caa0ed83
klyderaposas <klydevivo@gmail.com>
2026-02-01 13:30:34 +0800

    Add CaptchaShortener custom element

---
+++ main.js
@@ -0,0 +1,23 @@
+class CaptchaShortener extends HTMLElement{
+  connectedCallback(){
+    const type =
+    this.getAttribute("type") || "null";
+    const frontkey =
+    this.getAttribute("frontkey") || "ERROR";
+    const callnormal =
+    this.getAttribute("callnormal") || "myFunction";
+    const callerror =
+    this.getAttribute("callerror") || "myErrorFunction";
+    const size =
+    this.getAttribute("size") || "normal";
+    const lang = 
+    this.getAttribute("lang") || "en";
+    const theme = 
+    this.getAttribute("theme") || "dark";
+    
+    this.innerHTML = `
+    <div class="${type}" data-sitekey="${frontkey}" data-theme="${theme}" data-callback="${callnormal}" data-error-callback="${callerror}" data-size="${size}" data-language="${lang}"></div>`;
+  }
+}
+customElement.define("captcha-shortener", 
+CaptchaShortener);
